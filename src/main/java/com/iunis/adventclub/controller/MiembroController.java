94280de5ee460d76973613896abbed2e6da8fd2c
jcvisk <78867161+jcvisk@users.noreply.github.com>
2021-04-08 17:33:41 -0500

    en el template agregando al sidebar los botones para el link a los tipos de inscripcion y en el controller agregnado un nuevo metodo para crear una nueva inscripcion

--- src/main/java/com/iunis/adventclub/controller/MiembroController.java
+++ src/main/java/com/iunis/adventclub/controller/MiembroController.java
@@ -1,15 +1,25 @@
 package com.iunis.adventclub.controller;
 
+import com.iunis.adventclub.domain.Miembro;
 import com.iunis.adventclub.service.MiembroService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class MiembroController {
+    /**
+     * Informacion que se necesita
+     * Nombre del club
+     * DatosPersonales
+     * DatoUbicacion
+     * DatosFamiliares
+     * Clase
+     */
 
     @Autowired
     MiembroService miembroService;
@@ -18,4 +28,11 @@ public class MiembroController {
     public String display(Model model){
         return "miembro";
     }
+
+    @PostMapping(value = "/miembroCreate")
+    public String create(Miembro miembro){
+        //miembroService.
+        return "miembro";
+    }
+
 }
